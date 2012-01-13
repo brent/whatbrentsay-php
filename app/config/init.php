a41ec456e747005ca99797464c3bdef8d0de5f81
Justin Edmund <justin@jedmund.com>
2012-01-12 23:11:57 -0800

    The app won't render if a template isn't set. Displays a pretty (albeit inline) error message.

--- app/config/init.php
+++ app/config/init.php
@@ -1,47 +1,47 @@
 <?php
     // App Settings ---------------------------------------------
-    
+
     // The blog title.
-    $title = "Site Title";
-    
+    $title = "";
+
     // The content filename.
     $filename = 'article';
-    
+
     # Preferred file format.
     $format = 'md';
     # $format = 'html';
-    
+
     # Template Folder
-    $template = "whatbrentsay";
-    
+    $template = "";
+
 
     // Directory Constants --------------------------------------
 
     # Articles subdirectory
     $base_dir = 'articles';
-    
+
     # Template folder
     $template = ($template != "") ? $template . "/" : "";
 
-    # Current file path    
+    # Current file path
     $cwd = dirname(dirname(__FILE__));
-    
+
     # Directory separator (Windows/Linux agnostic)
     defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-    
+
     # Application directories
     defined('APP') ? null : define('APP', $cwd.DS);
     defined('LIB') ? null : define('LIB', APP.'lib'.DS);
-    
-    
+
+
     // Libraries ------------------------------------------------
-    
+
     # Markdown
     require_once(LIB.'markdown'.DS.'markdown-extra.php');
 
     # Twig
     require_once(LIB.DS.'Twig'.DS.'Autoloader.php');
-    
+
 	Twig_Autoloader::register();
 	$loader = new Twig_Loader_Filesystem(APP.'templates'.DS.$template);
 	$twig = new Twig_Environment($loader);
--- index.php
+++ index.php
@@ -1,34 +1,43 @@
 <?php				
 	require('app/config/init.php');
 	
-	// array of all articles in the base_dir
-	$articles = scandir($base_dir);
+	// If the user has set a template, render the index.
 	
-	foreach($articles as $article) {
-		
-		// only operate on folders that have metadata.json files 
-		if(file_exists("{$base_dir}/{$article}/metadata.json")) {
-		
-			$file = file_get_contents("{$base_dir}/{$article}/metadata.json");
-			$article_data = json_decode($file);
-			
-			// grab all the object variables then put them in an array
-			$article_data = get_object_vars($article_data);
-			
-			// add article root path to article data array
-			$article_data["base_path"] = $base_dir."/".$article."/";
-			
-			// yay, multidimensional arrays
-			$articles_array[] = $article_data;
-		}
-	}
-	
-	// reverse sort the array by each article's id
-	arsort($articles_array);
-	
-	$vars['title'] = $title;
-	$vars['articles'] = $articles_array;
-	
-	$template = $twig->loadTemplate('index.html');
-    echo $template->render($vars);
+	if ($template) {
+    	// array of all articles in the base_dir
+    	$articles = scandir($base_dir);
+    	
+    	foreach($articles as $article) {
+    		
+    		// only operate on folders that have metadata.json files 
+    		if(file_exists("{$base_dir}/{$article}/metadata.json")) {
+    		
+    			$file = file_get_contents("{$base_dir}/{$article}/metadata.json");
+    			$article_data = json_decode($file);
+    			
+    			// grab all the object variables then put them in an array
+    			$article_data = get_object_vars($article_data);
+    			
+    			// add article root path to article data array
+    			$article_data["base_path"] = $base_dir."/".$article."/";
+    			
+    			// yay, multidimensional arrays
+    			$articles_array[] = $article_data;
+    		}
+    	}
+    	
+    	// reverse sort the array by each article's id
+    	arsort($articles_array);
+    	
+    	$vars['title'] = $title;
+    	$vars['articles'] = $articles_array;
+    	
+    	$template = $twig->loadTemplate('index.html');
+        echo $template->render($vars);
+    } else {
+        echo "<div style='margin: 60px auto; width: 500px'>";
+        echo "<h1 style='font-family: sans-serif; font-weight: 300;'>You need to configure your app.</h1>";
+        echo "<p style='font: 16px/1.5 sans-serif; font-weight: 300;'>Go to the <span style='background: #efefef; padding: 3px; font-family: monospace'>app/</span> directory and set your template path and site title.</p>";
+        echo "</div>";
+    }
 ?>
