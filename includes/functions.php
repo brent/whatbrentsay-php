cc5ec3c0570c16ddc78f2d93398a45350115037f
Brent Meyer <brent.e.meyer@gmail.com>
2012-01-12 23:03:33 -0500

    added clean up function for titles with underscores

--- includes/functions.php
+++ includes/functions.php
@@ -25,7 +25,6 @@ function get_content_with_metadata($content_dir) {
 	}
 	// reverse sort the array by content id
 	arsort($content_array);
-	
 	return($content_array);
 }
 
@@ -34,26 +33,44 @@ function get_content_no_metadata($content_dir) {
 	$all_content = scandir($content_dir);
 	
 	foreach($all_content as $content) {
-	
+		
+		// sets $content to the name of the folder
 		$content_data['folder_name'] = $content;
-			
+		
+		// open the directory the content is in
 		$single_dir = opendir("{$content_dir}/{$content}");
 	
+		// read the files in the directory, then look for
+		// files with a .txt extension and add that file's
+		// name to the $content_data array
 		while(($file = readdir($single_dir)) !== false) {
-			if(preg_match("/.+txt/", $file, $matches)) {
+			
+			$regex = "/(.+txt|.+md)/";
+			
+			if(preg_match($regex, $file, $matches)) {
 				$content_data['title'] = $matches[0];
 			} else {
 				$content_data['title'] = null;
 			}
 		}
+		
+		// ensures you only return folders with .txt files
 		if(isset($content_data['title'])) {
+			$content_data['title'] = cleanup_title($content_data['title']);
 			$content_array[] = $content_data;
 		}
 	
 	}
 	
 	arsort($content_array);
-	
 	return($content_array);
+	
+}
+
+function cleanup_title($title) {
+	
+	$no_underscores = preg_replace("/_+/", " ", $title);
+	$good_title = ucwords($no_underscores);
+	return $good_title;
 	
 }
